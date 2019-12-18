6b6844094ba8902d6257af29236e817f6387f8fd
Robin Becker <robin.becker@live.se>
2019-12-18 08:47:55 +0100

    day 7 rollback to Int

--- D7.kt
+++ D7.kt
@@ -1,5 +1,4 @@
 import java.io.File
-import java.math.BigInteger
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -8,18 +7,18 @@ import kotlin.collections.ArrayList
 class D7 {
     companion object {
         fun run() {
-            val scanner: Scanner = Scanner(File("src/d7.txt"))
-            val array: ArrayList<BigInteger> = scanner.nextLine().split(",").map { t -> BigInteger.valueOf(t.toLong()) } as ArrayList<BigInteger>
-            val testArray = ArrayList<BigInteger>(listOf(3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33,
-                    1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0).map { t -> BigInteger.valueOf(t.toLong()) })
-            val phaseSeq = listOf(0, 1, 2, 3, 4).map { t -> BigInteger.valueOf(t.toLong()) }
+            val scanner = Scanner(File("src/d7.txt"))
+            val array: ArrayList<Int> = scanner.nextLine().split(",").map { t -> t.toInt() } as ArrayList<Int>
+            val testArray = ArrayList<Int>(listOf(3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33,
+                    1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0))
+            val phaseSeq = listOf(0, 1, 2, 3, 4)
 
-            var max: BigInteger
-            var maxPerm: List<BigInteger>? = null
+            var max: Int
+            var maxPerm: List<Int>? = null
 
             /*// Part 1
             for (permutation in permute(phaseSeq)) {
-                var input = BigInteger.ZERO // First input
+                var input = 0 // First input
                 for (phase in permutation) {
                     input = opComputer(array, phase, input)
                 }
@@ -37,24 +36,21 @@ class D7 {
                     27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5)
             val ampList = "ABCDE".split("").subList(1, "ABCDE".length + 1)
 
-            val computers: ArrayList<Pair<Int, ArrayList<BigInteger>>> = ArrayList()
+            val computers: ArrayList<Pair<Int, ArrayList<Int>>> = ArrayList()
 
-            max = BigInteger.ZERO
+            max = 0
 
-            permutation@ for (permutation in permute(phaseSeq2)) {
-                println("\nPermutation: " + permutation)
+            for (permutation in permute(phaseSeq2)) {
                 var ampListIterator = ampList.listIterator()
                 for (i in phaseSeq2) {
-                    computers.add(Pair(0, ArrayList<BigInteger>(array.map { t -> t })))
+                    computers.add(Pair(0, ArrayList<Int>(array)))
                 }
-                var input = BigInteger.ZERO // First input
-                val oldInput: BigInteger = BigInteger.ZERO
+                var input = 0 // First input
 
                 for (i in permutation.indices) {
-                    val answerPair = opComputer(computers[i].second, permutation[i].toBigInteger(), input, startPos = 0)
+                    val answerPair = opComputer(computers[i].second, permutation[i], input, startPos = 0)
                     computers[i] = computers[i].copy(first = answerPair.first)
                     input = answerPair.second
-                    println(ampListIterator.next() + ": " + input)
                     if (!ampListIterator.hasNext())
                         ampListIterator = ampList.listIterator()
                 }
@@ -66,16 +62,15 @@ class D7 {
 
                     max = if (max > input) max else input
                     if (max == input)
-                        maxPerm = permutation as ArrayList<BigInteger>
+                        maxPerm = permutation as ArrayList<Int>
 
-                    if (input == -1.toBigInteger()) {
+                    if (input == -1) {
                         computers.removeAt(currentAmp)
                         currentAmp--
                     }
                     currentAmp++
                     if (currentAmp == computers.size)
                         currentAmp = 0
-                    println(ampListIterator.next() + ": " + input)
                     if (!ampListIterator.hasNext())
                         ampListIterator = ampList.listIterator()
                 }
@@ -86,14 +81,13 @@ class D7 {
             println("with permutation: " + maxPerm)
         }
 
-        fun opComputer(array: ArrayList<BigInteger>, vararg input: BigInteger, startPos: Int): Pair<Int, BigInteger> {
+        fun opComputer(array: ArrayList<Int>, vararg input: Int, startPos: Int): Pair<Int, Int> {
             var stepSize: Int
             var inputPos = 0
-//            val outputArray: ArrayList<BigInteger> = ArrayList()
 
             var pos = startPos
             while (pos < array.size) {
-                val data = array[pos].toInt()
+                val data = array[pos]
                 var opcode = data
                 var modes = ArrayList<String>(listOf("0", "0"))
 
@@ -106,82 +100,71 @@ class D7 {
                         ArrayList(listOf("0", s.substring(0, 1)))
                 }
 
-                print("" + opcode + " ")
                 when (opcode) {
                     1 -> {
-                        val val1 = if (modes[1] == "1") array[pos + 1] else array[array[pos + 1].toInt()]
-                        val val2 = if (modes[0] == "1") array[pos + 2] else array[array[pos + 2].toInt()]
-                        array[array[pos + 3].toInt()] = val1 + val2
+                        val val1 = if (modes[1] == "1") array[pos + 1] else array[array[pos + 1]]
+                        val val2 = if (modes[0] == "1") array[pos + 2] else array[array[pos + 2]]
+                        array[array[pos + 3]] = val1 + val2
                         stepSize = 4
                     }
                     2 -> {
-                        val val1 = if (modes[1] == "1") array[pos + 1] else array[array[pos + 1].toInt()]
-                        val val2 = if (modes[0] == "1") array[pos + 2] else array[array[pos + 2].toInt()]
-                        array[array[pos + 3].toInt()] = val1 * val2
+                        val val1 = if (modes[1] == "1") array[pos + 1] else array[array[pos + 1]]
+                        val val2 = if (modes[0] == "1") array[pos + 2] else array[array[pos + 2]]
+                        array[array[pos + 3]] = val1 * val2
                         stepSize = 4
                     }
                     3 -> {
-                        try {
-                            array[array[pos + 1].toInt()] = input[inputPos]
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                        }
-//                        println("opcode:" + input[inputPos])
-//                        if (input.size > 1)
+                        array[array[pos + 1]] = input[inputPos]
                         inputPos++
 
                         stepSize = 2
                     }
                     4 -> {
-                        val value = BigInteger.valueOf((if (modes[1] == "0") array[array[pos + 1].toInt()] else array[pos + 1]).toLong())
+                        val value = (if (modes[1] == "0") array[array[pos + 1]] else array[pos + 1])
                         return Pair(pos + 2, value)
-//                        println(value)
-//                        outputArray.add(value)
-//                        stepSize = 2
                     }
                     5 -> {
-                        val val1 = if (modes[1] == "1") array[pos + 1] else array[array[pos + 1].toInt()]
-                        if (val1 != BigInteger.ZERO) {
-                            pos = (if (modes[0] == "1") array[pos + 2] else array[array[pos + 2].toInt()]).toInt()
+                        val val1 = if (modes[1] == "1") array[pos + 1] else array[array[pos + 1]]
+                        if (val1 != 0) {
+                            pos = (if (modes[0] == "1") array[pos + 2] else array[array[pos + 2]])
                             stepSize = 0
                         } else {
                             stepSize = 3
                         }
                     }
                     6 -> {
-                        val val1 = if (modes[1] == "1") array[pos + 1] else array[array[pos + 1].toInt()]
-                        if (val1 == BigInteger.ZERO) {
-                            pos = (if (modes[0] == "1") array[pos + 2] else array[array[pos + 2].toInt()]).toInt()
+                        val val1 = if (modes[1] == "1") array[pos + 1] else array[array[pos + 1]]
+                        if (val1 == 0) {
+                            pos = (if (modes[0] == "1") array[pos + 2] else array[array[pos + 2]])
                             stepSize = 0
                         } else {
                             stepSize = 3
                         }
                     }
                     7 -> {
-                        val val1 = if (modes[1] == "1") array[pos + 1] else array[array[pos + 1].toInt()]
-                        val val2 = if (modes[0] == "1") array[pos + 2] else array[array[pos + 2].toInt()]
+                        val val1 = if (modes[1] == "1") array[pos + 1] else array[array[pos + 1]]
+                        val val2 = if (modes[0] == "1") array[pos + 2] else array[array[pos + 2]]
 
-                        array[array[pos + 3].toInt()] = if (val1 < val2) BigInteger.ONE else BigInteger.ZERO
+                        array[array[pos + 3]] = if (val1 < val2) 1 else 0
                         stepSize = 4
 
                     }
                     8 -> {
-                        val val1 = if (modes[1] == "1") array[pos + 1] else array[array[pos + 1].toInt()]
-                        val val2 = if (modes[0] == "1") array[pos + 2] else array[array[pos + 2].toInt()]
+                        val val1 = if (modes[1] == "1") array[pos + 1] else array[array[pos + 1]]
+                        val val2 = if (modes[0] == "1") array[pos + 2] else array[array[pos + 2]]
 
-                        array[array[pos + 3].toInt()] = if (val1 == val2) BigInteger.ONE else BigInteger.ZERO
+                        array[array[pos + 3]] = if (val1 == val2) 1 else 0
                         stepSize = 4
                     }
                     99 ->
-                        return Pair(pos + 1, BigInteger.valueOf(-1))
+                        return Pair(pos + 1, -1)
                     else -> {
                         stepSize = 1
                     }
                 }
                 pos += stepSize
             }
-            println("End of opComputer")
-            return Pair(0, 0.toBigInteger())
+            return Pair(0, 0)
         }
 
         fun <T> permute(input: List<T>): List<List<T>> {
