bff66c72f753c8013bc0938c8d4a142fce38b497
Robin Becker <robin.becker@live.se>
2019-12-23 00:15:56 +0100

    day 10 part 2

--- D10.kt
+++ D10.kt
@@ -2,7 +2,6 @@ import java.io.File
 import java.lang.Integer.max
 import java.util.*
 import kotlin.collections.ArrayList
-import kotlin.collections.HashSet
 
 class D10 {
     companion object {
@@ -27,64 +26,94 @@ class D10 {
             }
 
             var max = 0
+            var maxPoint = Pair(0, 0)
             for (y in 0 until stars.size) {
                 for (xPos in 0 until stars[y].size) {
                     val x = stars[y][xPos]
                     var counter = 0
-                    // One neighbour-set for each quadrant
-                    val neighbours: ArrayList<HashSet<Double>> = ArrayList(listOf(HashSet(), HashSet(), HashSet(), HashSet()))
-                    var hasAbove = false
-                    var hasBelow = false
-                    // To the left
-                    if (stars[y].filter { c -> c < x }.toIntArray().size >= 1) counter++
-                    // To the right
-                    if (stars[y].filter { c -> c > x }.toIntArray().size >= 1) counter++
-                    for (subY in 0 until y) {
-                        for (subXPos in 0 until stars[subY].size) {
-                            val subX = stars[subY][subXPos]
-                            if (subY != y && subX == x)
-                                hasAbove = true
-                            else {
-                                val relativeX = x - subX
-                                val relativeY = y - subY
-                                val ratio = relativeX.toDouble() / relativeY
-                                if (subX < x) {
-                                    neighbours[0].add(ratio)
-                                } else {
-                                    neighbours[1].add(ratio)
-                                }
-                            }
-                        }
-                    }
-                    for (subY in y + 1 until stars.size) {
-                        for (subXPos in 0 until stars[subY].size) {
-                            val subX = stars[subY][subXPos]
-                            if (subY != y && subX == x)
-                                hasBelow = true
-                            else {
-                                val relativeX = x - subX
-                                val relativeY = y - subY
-                                val ratio = relativeX.toDouble() / relativeY
-                                if (subX < x) {
-                                    neighbours[2].add(ratio)
-                                } else {
-                                    neighbours[3].add(ratio)
-                                }
-                            }
-                        }
-                    }
-                    if (hasAbove) counter++
-                    if (hasBelow) counter++
 
+                    val neighbours = detect(stars, x, y)
                     for (neighbour in neighbours) {
                         counter += neighbour.size
                     }
                     max = max(max, counter)
                     if (counter == max)
-                        println("New max: $counter: $x,$y")
+                        maxPoint = Pair(x, y)
                 }
             }
-            println("Part 1: $max")
+            println("Part 1: $max at $maxPoint")
+
+            val x = maxPoint.first
+            val y = maxPoint.second
+            var vaporCounter = 0
+            outer@ while (vaporCounter < 200) {
+                val neighbors = detect(stars, x = maxPoint.first, y = maxPoint.second)
+                for (neighbor in neighbors) {
+                    for (entry in neighbor.entries.sortedWith(kotlin.Comparator { ent1, ent2 ->
+                        ent2.key.compareTo(ent1.key)
+                    })) {
+                        stars[entry.value.second].remove(entry.value.first)
+                        vaporCounter++
+                        if (vaporCounter == 200) {
+                            println("Part 2: ${entry.value} = ${entry.value.first * 100 + entry.value.second}")
+                            break@outer
+                        }
+                    }
+                }
+            }
+        }
+
+        fun detect(stars: ArrayList<ArrayList<Int>>, x: Int, y: Int): ArrayList<HashMap<Double, Pair<Int, Int>>> {
+            // One neighbour-set for each quadrant
+            val neighbours: ArrayList<HashMap<Double, Pair<Int, Int>>> = ArrayList(listOf(HashMap(), HashMap(), HashMap(), HashMap()))
+            for (subY in y - 1 downTo 0) {
+                for (subXPos in 0 until stars[subY].size) {
+                    val subX = stars[subY][subXPos]
+                    val coordinates = Pair(subX, subY)
+                    val relativeX = subX - x
+                    val relativeY = subY - y
+                    val ratio = relativeX.toDouble() / relativeY
+
+                    if (relativeX == 0) {
+                        neighbours[0].putIfAbsent(ratio, coordinates)
+                        continue
+                    } else {
+                        if (subX < x) {
+                            neighbours[3].putIfAbsent(ratio, coordinates)
+                        } else {
+                            neighbours[0].putIfAbsent(ratio, coordinates)
+                        }
+                    }
+                }
+            }
+            val rightSide = stars[y].filter { c -> c > x }
+            if (!rightSide.isEmpty())
+                neighbours[1].put(Double.MAX_VALUE, Pair(rightSide.first(), y))
+
+            for (subY in y + 1 until stars.size) {
+                for (subXPos in 0 until stars[subY].size) {
+                    val subX = stars[subY][subXPos]
+                    val coordinates = Pair(subX, subY)
+                    val relativeX = subX - x
+                    val relativeY = subY - y
+                    val ratio = relativeX.toDouble() / relativeY
+
+                    if (relativeX == 0) {
+                        neighbours[2].putIfAbsent(ratio, coordinates)
+                        continue
+                    } else {
+                        if (subX < x) {
+                            neighbours[2].putIfAbsent(ratio, coordinates)
+                        } else {
+                            neighbours[1].putIfAbsent(ratio, coordinates)
+                        }
+                    }
+                }
+            }
+            val leftSide = stars[y].filter { c -> c < x }
+            if (!leftSide.isEmpty())
+                neighbours[3].put(Double.MAX_VALUE, Pair(leftSide.last(), y))
+            return neighbours
         }
     }
 }
